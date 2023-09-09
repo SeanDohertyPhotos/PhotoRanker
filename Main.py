d97f2aae564b791946666f191f6ddac7d46241b5
smaster7772 <Smaster7772@gmail.com>
2023-09-09 12:04:08 -0500

    Enhanced PhotoRanker with randomized image selection, optimized loading, keyboard navigation, improved error handling, and GUI adjustments for a swift and user-friendly experience.
    Randomized Image Selection:
    
    Introduced randomness to select image pairs for comparison.
    Allowed for a mix of top-rated, unrated, and other images.
    Randomized the side on which the "highly-rated" image appears to prevent bias.
    Image Preloading:
    
    Added a preloading mechanism to improve image loading times.
    Used a separate thread to preload images into a queue.
    Fetching the next images for comparison from the queue to achieve faster transitions.
    Keyboard Navigation:
    
    Integrated keyboard controls for rating:
    Left Arrow: Rate left image as better.
    Right Arrow: Rate right image as better.
    Escape: Exit the program.
    Error Handling:
    
    Addressed the DecompressionBombError for large images by increasing the MAX_IMAGE_PIXELS value.
    Optimization:
    
    Modified the unrated image count calculation to use a more efficient approach.
    Updated the preloading mechanism to ensure faster transitions between images after rating.
    Code Refinement:
    
    Removed redundant functions and streamlined existing ones.
    Introduced helper functions for better code organization.
    Minor Changes:
    
    Adjusted the GUI layout, labels, and buttons for better usability.
    Updated the Elo rating adjustment mechanism.

--- Main.py
+++ Main.py
@@ -5,91 +5,100 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import rawpy
-import numpy as np
+from queue import Queue
+from threading import Thread
 
 RATINGS_FILE = "elo_ratings.json"
 TOP_RANK_COUNT = 10
 unrated_label = None
+Image.MAX_IMAGE_PIXELS = None  # To handle large images
 
 def open_image(img_path):
-    # Check if the image is a DNG file, and use rawpy to open it if so
-    if img_path.lower().endswith('.dng'):
-        with rawpy.imread(img_path) as raw:
-            return Image.fromarray(raw.postprocess())
-    else:
-        return Image.open(img_path)
+    return Image.open(img_path) if not img_path.lower().endswith('.dng') else Image.fromarray(rawpy.imread(img_path).postprocess())
 
 def get_images_from_folder(folder_path):
-    print("Getting images from folder...")  # Debug print
-    images = []
-    for subdir, _, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith(('jpg', 'png', 'jpeg', 'dng')):  # Include 'dng' in the tuple
-                images.append(os.path.join(subdir, file))
-    print(f"Found {len(images)} images.")  # Debug print
-    return images
+    return [os.path.join(subdir, file) for subdir, _, files in os.walk(folder_path) for file in files if file.lower().endswith(('jpg', 'png', 'jpeg', 'dng'))]
 
-def update_elo_rank(winner_elo, loser_elo):
-    K = 32
+def update_elo_rank(winner_elo, loser_elo, K):
     expected_winner = 1 / (1 + 10 ** ((loser_elo - winner_elo) / 400))
-    expected_loser = 1 / (1 + 10 ** ((winner_elo - loser_elo) / 400))
     winner_elo += K * (1 - expected_winner)
-    loser_elo += K * (0 - expected_loser)
+    loser_elo += K * (expected_winner - 1)
     return winner_elo, loser_elo
-    save_ratings()
+
+def get_unrated_count():
+    return sum(1 for img in images if elo_ratings[os.path.basename(img)]['rating'] == 1200)
 
 def select_winner(left_win):
     global image1, image2
-    winner = image1 if left_win else image2
-    loser = image2 if left_win else image1
-    filename_winner = os.path.basename(winner)
-    filename_loser = os.path.basename(loser)
-    elo_ratings[filename_winner]['rating'], elo_ratings[filename_loser]['rating'] = update_elo_rank(elo_ratings[filename_winner]['rating'], elo_ratings[filename_loser]['rating'])
+    winner, loser = (image1, image2) if left_win else (image2, image1)
+    filename_winner, filename_loser = os.path.basename(winner), os.path.basename(loser)
+
+    elo_ratings[filename_winner]['compared'] += 1
+    elo_ratings[filename_loser]['compared'] += 1
+
+    winner_elo, loser_elo = elo_ratings[filename_winner]['rating'], elo_ratings[filename_loser]['rating']
+    K = 32 if abs(winner_elo - loser_elo) < 100 else 16
+    winner_elo, loser_elo = update_elo_rank(winner_elo, loser_elo, K)
+
+    elo_ratings[filename_winner]['rating'] = winner_elo
+    elo_ratings[filename_loser]['rating'] = loser_elo
+    
+    unrated_label.config(text=f"Unrated Images in Current Folder: {get_unrated_count()}")
+    
     show_next_images()
 
 def show_next_images():
     global image1, image2
-    # Sort the images based on their rating, and select two images with a bias towards higher ratings
-    weighted_images = sorted(images, key=lambda img: elo_ratings[os.path.basename(img)]['rating'], reverse=True)
-    image1 = random.choices(weighted_images, weights=[i + 1 for i in range(len(weighted_images))], k=1)[0]
-    image2 = random.choice([img for img in weighted_images if img != image1])
+    if not preloaded_images.empty():
+        img1_path, img2_path, left_img, right_img = preloaded_images.get()
+        image1, image2 = img1_path, img2_path
+        left_photo, right_photo = ImageTk.PhotoImage(left_img), ImageTk.PhotoImage(right_img)
+        left_label.config(image=left_photo)
+        left_label.image = left_photo
+        right_label.config(image=right_photo)
+        right_label.image = right_photo
+        return
+
+    # If there are no preloaded images (though there should be), fall back to existing logic
+    top_images = sorted([img for img in images if elo_ratings[os.path.basename(img)]['rating'] > 1200], 
+                        key=lambda img: elo_ratings[os.path.basename(img)]['rating'], reverse=True)[:10]
+    unrated_images = [img for img in images if elo_ratings[os.path.basename(img)]['rating'] == 1200]
+    other_images = [img for img in images if img not in top_images and img not in unrated_images]
+
+    choices = ["top", "unrated", "other"]
+    image1_choice, image2_choice = random.choice(choices), random.choice(choices)
+
+    if image1_choice == "top": image1 = random.choice(top_images)
+    elif image1_choice == "unrated": image1 = random.choice(unrated_images) if unrated_images else random.choice(other_images)
+    else: image1 = random.choice(other_images)
+
+    if image2_choice == "top": image2 = random.choice([img for img in top_images if img != image1])
+    elif image2_choice == "unrated": image2 = random.choice([img for img in unrated_images if img != image1]) if unrated_images else random.choice(other_images)
+    else: image2 = random.choice([img for img in other_images if img != image1])
+
+    if random.choice([True, False]): image1, image2 = image2, image1
+
     update_images(image1, image2)
-    
-    # Count the number of unrated images (those with the default rating) in the current folder
-    unrated_count = sum(1 for img in images if elo_ratings[os.path.basename(img)]['rating'] == 1200)
-    unrated_label.config(text=f"Unrated Images in Current Folder: {unrated_count}")  # Update the unrated_label with the count
 
 def resize_image(img, width, height):
     img_ratio = img.width / img.height
     target_ratio = width / height
-    if img_ratio > target_ratio:
-        new_width = width
-        new_height = int(width / img_ratio)
-    else:
-        new_height = height
-        new_width = int(height * img_ratio)
+    new_width = width if img_ratio > target_ratio else int(height * img_ratio)
+    new_height = int(width / img_ratio) if img_ratio > target_ratio else height
     return img.resize((new_width, new_height), Image.LANCZOS)
 
 def update_images(img1, img2):
-    left_img = open_image(img1)
-    right_img = open_image(img2)
-    
-    left_img = resize_image(left_img, image_width, image_height)
-    right_img = resize_image(right_img, image_width, image_height)
-    left_photo = ImageTk.PhotoImage(left_img)
-    right_photo = ImageTk.PhotoImage(right_img)
+    left_img, right_img = open_and_resize_image(img1), open_and_resize_image(img2)
+    left_photo, right_photo = ImageTk.PhotoImage(left_img), ImageTk.PhotoImage(right_img)
     left_label.config(image=left_photo)
     left_label.image = left_photo
     right_label.config(image=right_photo)
     right_label.image = right_photo
 
 def on_key(event):
-    if event.keysym == 'Left':
-        select_winner(True)
-    elif event.keysym == 'Right':
-        select_winner(False)
-    elif event.keysym == 'Escape':
-        quit_program()
+    if event.keysym == 'Left': select_winner(True)
+    elif event.keysym == 'Right': select_winner(False)
+    elif event.keysym == 'Escape': quit_program()
 
 def save_ratings():
     with open(RATINGS_FILE, 'w') as file:
@@ -99,7 +108,6 @@ def load_ratings():
     if os.path.exists(RATINGS_FILE):
         with open(RATINGS_FILE, 'r') as file:
             loaded_ratings = json.load(file)
-            # Normalize the loaded ratings
             for key, value in loaded_ratings.items():
                 if isinstance(value, (int, float)):
                     loaded_ratings[key] = {'path': '', 'rating': value}
@@ -115,55 +123,77 @@ def view_rankings():
 def view_top_ranked():
     top_rank_window = tk.Toplevel(root)
     top_rank_window.title('Top Ranked Images')
-    
-    # Create a canvas inside the top_rank_window
     canvas = tk.Canvas(top_rank_window)
     canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-    
-    # Add a scrollbar to the canvas
     scrollbar = tk.Scrollbar(top_rank_window, command=canvas.yview)
     scrollbar.pack(side=tk.LEFT, fill=tk.Y)
     canvas.configure(yscrollcommand=scrollbar.set)
-    
-    # Create a frame inside the canvas
     frame = tk.Frame(canvas)
     canvas.create_window((0, 0), window=frame, anchor='nw')
-
     for filename, details in sorted(elo_ratings.items(), key=lambda x: x[1]['rating'], reverse=True)[:TOP_RANK_COUNT]:
         img_path = details["path"]
-        rating = round(details["rating"])  # Round the rating to the nearest integer
-        if os.path.exists(img_path):  # Check if the image file exists
-            img = open_image(img_path)  # Use open_image function here
-            img = resize_image(img, image_width, image_height)
+        rating = round(details["rating"])
+        if os.path.exists(img_path):
+            img = open_and_resize_image(img_path)
             photo = ImageTk.PhotoImage(img)
             image_label = tk.Label(frame, image=photo)
             image_label.image = photo
             image_label.pack()
-            tk.Label(frame, text=f'Rating: {rating}').pack()  # Use the rounded rating
+            tk.Label(frame, text=f'Rating: {rating}').pack()
         else:
-            tk.Label(frame, text=f'{filename} not found. Rating: {rating}').pack()  # Use the rounded rating
-            
-    # Update the scroll region to match the size of the frame
+            tk.Label(frame, text=f'{filename} not found. Rating: {rating}').pack()
     frame.update_idletasks()
     canvas.config(scrollregion=canvas.bbox("all"))
-
 
 def quit_program():
     save_ratings()
     root.quit()
 
+def open_and_resize_image(img_path):
+    img = open_image(img_path)
+    return resize_image(img, image_width, image_height)
+
+def get_next_images_for_preload():
+    top_images = sorted([img for img in images if elo_ratings[os.path.basename(img)]['rating'] > 1200], 
+                        key=lambda img: elo_ratings[os.path.basename(img)]['rating'], reverse=True)[:10]
+    unrated_images = [img for img in images if elo_ratings[os.path.basename(img)]['rating'] == 1200]
+    other_images = [img for img in images if img not in top_images and img not in unrated_images]
+
+    choices = ["top", "unrated", "other"]
+    image1_choice, image2_choice = random.choice(choices), random.choice(choices)
+
+    if image1_choice == "top": img1 = random.choice(top_images)
+    elif image1_choice == "unrated": img1 = random.choice(unrated_images) if unrated_images else random.choice(other_images)
+    else: img1 = random.choice(other_images)
+
+    if image2_choice == "top": img2 = random.choice([img for img in top_images if img != img1])
+    elif image2_choice == "unrated": img2 = random.choice([img for img in unrated_images if img != img1]) if unrated_images else random.choice(other_images)
+    else: img2 = random.choice([img for img in other_images if img != img1])
+
+    return img1, img2
+
+def preload_images():
+    while True:
+        img1, img2 = get_next_images_for_preload()
+        left_img = open_and_resize_image(img1)
+        right_img = open_and_resize_image(img2)
+        preloaded_images.put((img1, img2, left_img, right_img))
+
 folder_path = filedialog.askdirectory(title='Select a folder containing images')
-print(f"Selected folder: {folder_path}")  # Debug print
 images = get_images_from_folder(folder_path)
 elo_ratings = load_ratings()
-print("Loaded ratings.")  # Debug print
 for image in images:
     filename = os.path.basename(image)
-    existing_entry = elo_ratings.get(filename, {'rating': 1200})
-    existing_rating = existing_entry if isinstance(existing_entry, (int, float)) else existing_entry.get('rating', 1200)
-    elo_ratings[filename] = {'path': image, 'rating': existing_rating}
-image1, image2 = None, None
+    existing_entry = elo_ratings.get(filename, {'rating': 1200, 'compared': 0})
+    elo_ratings[filename] = {
+        'path': image,
+        'rating': existing_entry.get('rating', 1200),
+        'compared': existing_entry.get('compared', 0)
+    }
+
 image_width, image_height = 800, 600
+preloaded_images = Queue(maxsize=5)
+Thread(target=preload_images, daemon=True).start()
 
 root = tk.Tk()
 root.title('Image Ranking')
@@ -187,7 +217,7 @@ view_top_button.pack(side=tk.LEFT, padx=5)
 quit_button = tk.Button(button_frame, text="Quit", command=quit_program, bg='gray', fg='white')
 quit_button.pack(side=tk.LEFT, padx=5)
 
-unrated_label = tk.Label(root, text="Unrated Images:", bg='black', fg='white')
+unrated_label = tk.Label(root, text=f"Unrated Images in Current Folder: {get_unrated_count()}", bg='black', fg='white')
 unrated_label.pack(side=tk.BOTTOM, pady=5)
 
 root.bind('<Left>', on_key)
@@ -195,5 +225,4 @@ root.bind('<Right>', on_key)
 root.bind('<Escape>', on_key)
 
 show_next_images()
-print("Running main loop...")  # Debug print
 root.mainloop()
